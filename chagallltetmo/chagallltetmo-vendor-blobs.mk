389049b3846862fe8f4fcdc4b6ecff83568bc3a9
Alexander Gemsa <alexander.gemsa@gmail.com>
2017-01-15 18:34:07 +0100

    Update for Devices i have

---
+++ chagallltetmo/chagallltetmo-vendor-blobs.mk
@@ -0,0 +1,25 @@
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# RIL
+PRODUCT_COPY_FILES += \
+    vendor/samsung/chagallltetmo/proprietary/lib/libsec-ril.so:system/lib/libsec-ril.so
+
+# Bluetooth firmware
+PRODUCT_COPY_FILES += \
+    vendor/samsung/chagallltetmo/proprietary/vendor/firmware/bcm4350_V0301.0595.hcd:system/vendor/firmware/bcm4350_V0301.0595.hcd
+
+# Sensors
+PRODUCT_COPY_FILES += \
+    vendor/samsung/chagallltetmo/proprietary/lib/hw/sensors.universal5420.so:system/lib/hw/sensors.universal5420.so
